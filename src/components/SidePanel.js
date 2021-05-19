753e4ca72748149f6b2312ecb32af1fd5d4cc236
Ameed Jadallah <ameed.jadallah@gmail.com>
2021-05-20 00:45:54 +0300

    feat(sidepanel): implement sidepanel cpmponent

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import classnames from "classnames";
+import { FaAngleDoubleLeft, FaAngleDoubleRight } from "react-icons/fa";
 
 import "./SidePanel.scss";
-import { FaAngleDoubleLeft, FaAngleDoubleRight } from "react-icons/fa";
+import data  from '../data/data.json';
 
 function SidePanel() {
   const [collapsed, setCollapced] = useState(false);
+  const userData = data.user;
 
   const toggleSidePanel = () => {
     setCollapced(!collapsed);
@@ -14,12 +16,32 @@ function SidePanel() {
   return (
     <div
       className={classnames(`side-panel`, {
-        'collapsed': collapsed,
+        collapsed: collapsed,
       })}
     >
-      <button onClick={toggleSidePanel}>
+      <button
+        className="btn collapsed-btn inline-flex justify-center items-center"
+        onClick={toggleSidePanel}
+      >
         {collapsed ? <FaAngleDoubleLeft /> : <FaAngleDoubleRight />}
       </button>
+      {!collapsed && (
+        <div className="side-panel__content">
+          <div className="profile-image">
+            <img src={userData.image} />
+          </div>
+          <div className="profile-info">
+            <div className="info-item flex justify-between">
+              <label>Name</label>
+              <div className="info-value">{userData.name}</div>
+            </div>
+            <div className="info-item flex justify-between">
+              <label>Age</label>
+              <div className="info-value">{userData.age} years old</div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
